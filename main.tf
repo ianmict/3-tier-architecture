2911e1f04e23a859e1a1cd54dee91ab8644f3c10
Ian Michael Tablizo <ianm.tablizo@gmail.com>
2024-11-04 01:02:17 -0700

    1

---
+++ main.tf
@@ -0,0 +1,134 @@
+terraform {
+  required_providers {
+    aws ={
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+locals {
+  reg1 = "us-east-1a"
+  reg2 = "us-east-1b"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+//VPC
+
+resource "aws_vpc" "3-tier-VPC" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "3-tier-VPC"
+  }
+}
+
+//Subnets in the VPC
+
+resource "aws_subnet" "public-subnet-1" {
+  vpc_id = aws_vpc.3-tier-VPC.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = local.reg1
+
+  tags = {
+    Name = "public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public-subnet-2" {
+  vpc_id = aws_vpc.3-tier-VPC.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = local.reg2
+
+  tags = {
+    Name = "public-subnet-2"
+  }
+}
+
+resource "aws_subnet" "private-subnet-1" {
+  vpc_id = aws_vpc.3-tier-VPC.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = local.reg1
+
+  tags = {
+    Name = "private-subnet-1"
+  }
+}
+
+resource "aws_subnet" "private-subnet-2" {
+  vpc_id = aws_vpc.3-tier-VPC.id
+  cidr_block = "10.0.4.0/24"
+  availability_zone = local.reg2
+
+  tags = {
+    Name = "private-subnet-2"
+  }
+}
+
+resource "aws_subnet" "private-subnet-3" {
+  vpc_id = aws_vpc.3-tier-VPC.id
+  cidr_block = "10.0.5.0/24"
+  availability_zone = local.reg1
+
+  tags = {
+    Name = "private-subnet-3"
+  }
+}
+
+resource "aws_subnet" "private-subnet-4" {
+  vpc_id = aws_vpc.3-tier-VPC.id
+  cidr_block = "10.0.6.0/24"
+  availability_zone = local.reg2
+
+  tags = {
+    Name = "private-subnet-4"
+  }
+}
+
+//security group
+
+resource "aws_security_group" "front-end-sg" {
+  name = "front-end-sg"
+  description = "allow all traffic from the internet"
+  vpc_id = aws_vpc.3-tier-VPC.id
+
+  tags = {
+    Name = "front-end-sg"
+  }
+}
+
+resource "aws_security_group" "logic-tier-sg" {
+  name = "logic-tier-sg"
+  description = "allows traffic from front-end and to database tier"
+  vpc_id = aws_vpc.3-tier-VPC.id
+
+  tags = {
+    Name = "logic-tier-sg"
+  }
+}
+
+resource "aws_security_group" "db-tier-sg" {
+  name = "db-tier-sg"
+  description = "allows traffic only from the logic tier"
+  vpc_id = aws_vpc.3-tier-VPC.id
+
+  tags = {
+    Name = "db-tier-sg"
+  }
+}
+
+//security group rules
+
+resource "aws_security_group_ingress_rule" "front-end-ingress" {
+  security_group_id = aws_security_group.front-end-sg.id
+  cidr_block = "0.0.0.0/0"
+  ip_protocol = "-1"
+}
+
+resource "aws_security_group_egress_rule" "front-end-egress" {
+  security_group_id = aws_security_group.front-end-sg.id
+  
+}
